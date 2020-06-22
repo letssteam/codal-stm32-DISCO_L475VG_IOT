2ba022b3c1c405b31cdbc22017bf13d40524e2f3
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2020-06-22 11:47:07 +0300

    Add a blink sample

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,5 +1,33 @@
+#include "wiring_time.h"
+#include "board.h"
+
+#include "STM32Pin.h"
+#include "STM32IotNode.h"
+
+using namespace codal;
+
+// Force init to be called *first*, i.e. before static object allocation.
+// Otherwise, statically allocated objects that need HAL may fail.
+__attribute__((constructor(101))) void premain()
+{
+  init();
+}
 
 int main()
 {
+    STM32IotNode iotnode;
+    int pin_state = 0;
 
+    while (1) {
+        pin_state = !pin_state;
+        if (pin_state) {
+            iotnode.io.led1.setDigitalValue(HIGH);
+            iotnode.io.led2.setDigitalValue(LOW);
+        } else {
+            iotnode.io.led1.setDigitalValue(LOW);
+            iotnode.io.led2.setDigitalValue(HIGH);
+        }
+        delay(500);
+        //iotnode.sleep(500);
+    }
 }

8fdf70ff99709d62e84f3ac16d46d5a22c7775fe
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2020-06-30 13:32:50 +0300

    Add GPIO example

---
+++ samples/Basic/GPIO/GPIOSample.cpp
@@ -0,0 +1,31 @@
+#include "Sensor.h"
+#include "GPIOSample.h"
+
+using namespace codal;
+
+void onSampleEvent(Event e){
+    int value = default_device_instance->io.A3.getAnalogValue();
+    default_device_instance->io.D5.setAnalogValue(value);
+    printf("A3 = %d\n", value);
+}
+
+
+void GPIOSample_main(){
+    codal::STM32IotNode iotNode;
+    printf("\n");
+    printf("*******************************************\n");
+    printf("*           Demonstration de I/O          *\n");
+    printf("*******************************************\n");
+    //iotNode.io.A1.setAnalogValue(512);
+    iotNode.sleep(1000);
+    system_timer_event_every(1000, ID_PIN_P0, SENSOR_UPDATE_NEEDED);
+    EventModel::defaultEventBus->listen(ID_PIN_P0, SENSOR_UPDATE_NEEDED, onSampleEvent);
+
+    while(1) {
+        iotNode.io.led1.setDigitalValue(1);
+        iotNode.sleep(1000);
+
+        iotNode.io.led1.setDigitalValue(0);
+        iotNode.sleep(1000);
+    }
+}
---
+++ samples/Basic/GPIO/GPIOSample.h
@@ -0,0 +1,12 @@
+#include "STM32IotNode.h"
+
+#ifndef GPIO_SAMPLE_H
+#define GPIO_SAMPLE_H
+
+#ifndef SAMPLE_MAIN
+    #define SAMPLE_MAIN GPIOSample_main
+#endif
+
+void GPIOSample_main();
+
+#endif

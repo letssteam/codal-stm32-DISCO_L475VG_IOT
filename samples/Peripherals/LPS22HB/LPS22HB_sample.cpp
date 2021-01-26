8fcf1e7b65f3ea5e5ce37fa4a545714fea6fab42
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-01-26 18:49:04 +0100

    Add LPS22HB sample

---
+++ samples/Peripherals/LPS22HB/LPS22HB_sample.cpp
@@ -0,0 +1,40 @@
+
+#include "LPS22HB_sample.h"
+
+using namespace codal;
+using namespace std;
+
+void lps22hbSample(){
+
+    STM32IotNode iotNode;
+    SSD1306_I2C oled( iotNode.i2c1, 0x78, 128, 64, false );
+    LPS22HB lps( iotNode.i2c2, 0xBA );
+
+
+    oled.init();
+    lps.init();
+
+    oled.fill(0);
+    oled.drawText( "LPS22HB Test !!", 16, 28, 1 );
+    oled.show();
+
+    iotNode.sleep( 2000 );
+
+    string pres;
+    string temp;
+
+    while(1){
+
+        pres = to_string( lps.getPressure() ).substr(0, 6);
+        temp = to_string( lps.getTemperature() ).substr(0, 4);
+
+        oled.fill(0);        
+        oled.drawText( "LPS22HB", 38, 0, 1 );
+        oled.drawText( "Pres : " + pres + "hPa", 0, 28, 1);
+        oled.drawText( "Temp : " + temp + " C", 0, 36, 1);
+        oled.show();
+
+        iotNode.sleep(500);
+    }
+
+}

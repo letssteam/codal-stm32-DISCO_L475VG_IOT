77e708cc3985e0395325eed5f286b6d831adeee4
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-02-01 18:44:27 +0100

    Add VL53L0x sample

---
+++ samples/Peripherals/VL53L0x/VL53L0x_sample.cpp
@@ -0,0 +1,38 @@
+
+#include "VL53L0x_sample.h"
+
+using namespace codal;
+using namespace std;
+
+void vlx53l0xSample(){
+
+    STM32IotNode iotNode;
+    SSD1306_I2C oled( iotNode.i2c1, 0x78, 128, 64, false );
+    VL53L0x vl( iotNode.i2c2,  iotNode.io.pc6, 0x52 );
+
+
+    oled.init();
+    vl.init();
+
+    oled.fill(0);
+    oled.drawText( "VL53L0x test !!", 24, 36, 1 );
+    oled.show();
+
+    iotNode.sleep( 2000 );
+
+    string dist;
+
+    while(1){
+
+        oled.fill(0);
+
+        dist = "Dist : " + to_string( vl.getDistance() ) + " mm";
+
+        oled.drawText( "VL53L0x test", 24, 0, 1 );
+        oled.drawText( dist, 0, 30, 1 );
+
+        oled.show();
+        iotNode.sleep(500);
+    }
+
+}
---
+++ samples/Peripherals/VL53L0x/VL53L0x_sample.h
@@ -0,0 +1,16 @@
+
+#ifndef __HTS221_SAMPLE_H__
+#define __HTS221_SAMPLE_H__
+
+#include "STM32IotNode.h"
+#include "ssd1306.h"
+#include "VL53L0x.h"
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN vlx53l0xSample
+#endif
+
+
+void vlx53l0xSample();
+
+#endif

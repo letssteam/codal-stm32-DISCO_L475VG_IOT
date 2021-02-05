0ec6025a478a0ddcd36e216d49693a7ad060f926
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-02-05 12:35:45 +0200

    Correct the indentation

--- samples/Basic/Blink/BlinkSample.cpp
+++ samples/Basic/Blink/BlinkSample.cpp
@@ -5,7 +5,7 @@ void BlinkSample_main(){
 	bool state = false;
 	while(true)
     {    
-		iotNode.io.led1.setDigitalValue((int)state);
+        iotNode.io.led1.setDigitalValue((int)state);
         iotNode.io.led2.setDigitalValue((int)!state);
         iotNode.sleep(1000);
         state = !state;

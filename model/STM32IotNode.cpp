c051b48de8a32ee504e13858e00e3f0db3c89f01
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-03-31 14:28:25 +0000

    Remove call to init() inside the constructor

--- model/STM32IotNode.cpp
+++ model/STM32IotNode.cpp
@@ -4,75 +4,70 @@ using namespace codal;
 
 void STM32IotNode_dmesg_flush();
 
-STM32IotNode *codal::default_device_instance = nullptr;
-
+STM32IotNode* codal::default_device_instance = nullptr;
 
 /**
-  * Constructor.
-  *
-  * Create a representation of a device, which includes member variables
-  * that represent various device drivers used to control aspects of the STM32 IOT node.
-  */
+ * Constructor.
+ *
+ * Create a representation of a device, which includes member variables
+ * that represent various device drivers used to control aspects of the STM32 IOT node.
+ */
 STM32IotNode::STM32IotNode()
-  : lowLevelTimer(TIM_MST, TIM_MST_IRQ),
-    timer(lowLevelTimer),
-    i2c1( io.sda, io.scl ),
-    i2c2( io.sda2, io.scl2 ),
-    serial( io.rx, io.tx ),
-    spi1(io.miso, io.mosi, io.sclk),
-    spi3(io.miso3, io.mosi3, io.sclk3),
-    hygrometer(DEVICE_ID_HUMIDITY, i2c2, 0xBE),
-    thermometer(DEVICE_ID_THERMOMETER, i2c2, 0xBE),
-    barometer(DEVICE_ID_PRESSURE, i2c2, 0xBA),
-    distance(DEVICE_ID_DISTANCE, i2c2, io.pc6, 0x52),
-    coordinateSpace(SIMPLE_CARTESIAN, true, COORDINATE_SPACE_ROTATED_180),
-    magnetometer(i2c2, 0x3C, coordinateSpace),
-    accelerometer(i2c2, 0xD4, coordinateSpace),
-    gyroscope(i2c2, 0xD4, coordinateSpace)
-    //buttonUSER(io.btnUser, DEVICE_ID_BUTTON_A, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW)
+    : lowLevelTimer(TIM_MST, TIM_MST_IRQ),
+      timer(lowLevelTimer),
+      i2c1(io.sda, io.scl),
+      i2c2(io.sda2, io.scl2),
+      serial(io.rx, io.tx),
+      spi1(io.miso, io.mosi, io.sclk),
+      spi3(io.miso3, io.mosi3, io.sclk3),
+      hygrometer(DEVICE_ID_HUMIDITY, i2c2, 0xBE),
+      thermometer(DEVICE_ID_THERMOMETER, i2c2, 0xBE),
+      barometer(DEVICE_ID_PRESSURE, i2c2, 0xBA),
+      distance(DEVICE_ID_DISTANCE, i2c2, io.pc6, 0x52),
+      coordinateSpace(SIMPLE_CARTESIAN, true, COORDINATE_SPACE_ROTATED_180),
+      magnetometer(i2c2, 0x3C, coordinateSpace),
+      accelerometer(i2c2, 0xD4, coordinateSpace),
+      gyroscope(i2c2, 0xD4, coordinateSpace),
+      buttonUSER(io.btnUser, DEVICE_ID_BUTTON_A, DEVICE_BUTTON_ALL_EVENTS, ACTIVE_LOW)
 {
     // Clear our status
-    status = 0;
+    status                  = 0;
     default_device_instance = this;
-    //this->init();
-
-    default_serial_debug = &serial;
+    default_serial_debug    = &serial;
 }
 
 /**
-  * Post constructor initialisation method.
-  *
-  * This call will initialised the scheduler, memory allocator and Bluetooth stack.
-  *
-  * This is required as the Bluetooth stack can't be brought up in a
-  * static context i.e. in a constructor.
-  *
-  * @code
-  * stm32IotNode.init();
-  * @endcode
-  *
-  * @note This method must be called before user code utilises any functionality
-  *       contained within the STM32IotNode class.
-  */
+ * Post constructor initialisation method.
+ *
+ * This call will initialised the scheduler, memory allocator and Bluetooth stack.
+ *
+ * This is required as the Bluetooth stack can't be brought up in a
+ * static context i.e. in a constructor.
+ *
+ * @code
+ * stm32IotNode.init();
+ * @endcode
+ *
+ * @note This method must be called before user code utilises any functionality
+ *       contained within the STM32IotNode class.
+ */
 int STM32IotNode::init()
 {
-    if ((status & DEVICE_INITIALIZED) != 0){
+    if ((status & DEVICE_INITIALIZED) != 0) {
         return DEVICE_NOT_SUPPORTED;
     }
 
     status |= DEVICE_INITIALIZED;
 
-    //codal_dmesg_set_flush_fn(STM32IotNode_dmesg_flush);    
-    
     // Bring up fiber scheduler.
     scheduler_init(messageBus);
 
-    for(int i = 0; i < DEVICE_COMPONENT_COUNT; i++)
-    {
-        if(CodalComponent::components[i] != nullptr){
+    for (int i = 0; i < DEVICE_COMPONENT_COUNT; i++) {
+        if (CodalComponent::components[i] != nullptr) {
             CodalComponent::components[i]->init();
         }
     }
+    codal_dmesg_set_flush_fn(STM32IotNode_dmesg_flush);
 
     status |= DEVICE_COMPONENT_STATUS_IDLE_TICK;
 
@@ -81,13 +76,27 @@ int STM32IotNode::init()
 
 WEAK int __io_putchar(int ch);
 
+void STM32IotNode::idleCallback()
+{
+    codal_dmesg_flush();
+}
+
+void STM32IotNode::sleep(uint32_t milliseconds)
+{
+    fiber_sleep(milliseconds);
+}
+
+CODAL_TIMESTAMP STM32IotNode::systemTime()
+{
+    return system_timer_current_time();
+}
+
 void STM32IotNode_dmesg_flush()
 {
 #if CONFIG_ENABLED(DMESG_SERIAL_DEBUG)
 #if DEVICE_DMESG_BUFFER_SIZE > 0
-    if (codalLogStore.ptr > 0 && default_device_instance != nullptr)
-    {
-        for (uint32_t i=0; i<codalLogStore.ptr; i++){
+    if (codalLogStore.ptr > 0 && default_device_instance != nullptr) {
+        for (uint32_t i = 0; i < codalLogStore.ptr; i++) {
             __io_putchar(codalLogStore.buffer[i]);
         }
         codalLogStore.ptr = 0;
--- model/STM32IotNode.h
+++ model/STM32IotNode.h
@@ -1,33 +1,29 @@
 #ifndef STM32_IOT_NODE_H
 #define STM32_IOT_NODE_H
 
-#include "stm32_def.h"
+#include "Button.h"
 #include "CodalComponent.h"
 #include "CodalDevice.h"
-#include "CodalFiber.h"
 #include "CodalDmesg.h"
-#include "codal-core/inc/types/Event.h"
-#include "codal-core/inc/driver-models/Timer.h"
-
-#include "NotifyEvents.h"
-#include "MessageBus.h"
-
-#include "Button.h"
-#include "STM32LowLevelTimer.h"
-#include "STM32IotNodeIO.h"
-#include "STM32I2C.h"
-#include "STM32Serial.h"
-#include "STM32SPI.h"
-
-#include "LIS3MDL_Magnetometer.h"
-#include "LSM6DSL_Accelerometer.h"
-#include "LSM6DSL_Gyroscope.h"
+#include "CodalFiber.h"
+#include "DumbSensor.h"
 #include "HTS221_Humidity.h"
 #include "HTS221_Temperature.h"
+#include "LIS3MDL_Magnetometer.h"
 #include "LPS22HB_Pressure.h"
+#include "LSM6DSL_Accelerometer.h"
+#include "LSM6DSL_Gyroscope.h"
+#include "MessageBus.h"
+#include "NotifyEvents.h"
+#include "STM32I2C.h"
+#include "STM32IotNodeIO.h"
+#include "STM32LowLevelTimer.h"
+#include "STM32SPI.h"
+#include "STM32Serial.h"
 #include "VL53L0X_Distance.h"
-
-
+#include "codal-core/inc/driver-models/Timer.h"
+#include "codal-core/inc/types/Event.h"
+#include "stm32_def.h"
 
 // Status flag values
 #define DEVICE_INITIALIZED 0x01
@@ -38,90 +34,76 @@
  * Represents the device as a whole, and includes member variables that represent various device drivers
  * used to control aspects of the STM32 IOT node.
  */
-namespace codal
-{
-    class STM32IotNode : public CodalComponent
-    {
-        public:
-            STM32LowLevelTimer lowLevelTimer;
-            Timer timer;
-            MessageBus     messageBus;
+namespace codal {
+class STM32IotNode : public CodalComponent {
+  public:
+    STM32LowLevelTimer lowLevelTimer;
+    Timer timer;
+    MessageBus messageBus;
 
-            STM32IotNodeIO io;
-            STM32I2C i2c1;//External on arduino stardard pins 
-            STM32I2C i2c2;//Internal only used by MEMS sensors 
-            STM32Serial serial;
-            STM32SPI spi1;
-            STM32SPI spi3;
+    STM32IotNodeIO io;
+    STM32I2C i2c1;  // External on arduino stardard pins
+    STM32I2C i2c2;  // Internal only used by MEMS sensors
+    STM32Serial serial;
+    STM32SPI spi1;
+    STM32SPI spi3;
 
-            HTS221_Humidity hygrometer;
-            HTS221_Temperature thermometer;
-            LPS22HB_Pressure barometer;
-            VL53L0X_Distance distance;
-            
-            CoordinateSpace coordinateSpace;
-            LIS3MDL_Magnetometer magnetometer;
-            LSM6DSL_Accelerometer accelerometer;
-            LSM6DSL_Gyroscope gyroscope;
+    DumbSensor dumbSensor;
+    HTS221_Humidity hygrometer;
+    HTS221_Temperature thermometer;
+    LPS22HB_Pressure barometer;
+    VL53L0X_Distance distance;
 
+    CoordinateSpace coordinateSpace;
+    LIS3MDL_Magnetometer magnetometer;
+    LSM6DSL_Accelerometer accelerometer;
+    LSM6DSL_Gyroscope gyroscope;
 
-            //Button buttonUSER;
-            
-            /**
-             * Constructor.
-             */
-            STM32IotNode();
+    Button buttonUSER;
 
-            /**
-             * Post constructor initialisation method.
-             */
-            int init() override final;
+    /**
+     * Constructor.
+     */
+    STM32IotNode();
 
-            /**
-             * Delay execution for the given amount of time.
-             *
-             * If the scheduler is running, this will deschedule the current fiber and perform
-             * a power efficient, concurrent sleep operation.
-             *
-             * If the scheduler is disabled or we're running in an interrupt context, this
-             * will revert to a busy wait.
-             *
-             * Alternatively: wait, wait_ms, wait_us can be used which will perform a blocking sleep
-             * operation.
-             *
-             * @param milliseconds the amount of time, in ms, to wait for. This number cannot be negative.
-             *
-             */
-            virtual void sleep(uint32_t milliseconds){
-                fiber_sleep(milliseconds);
-            }
+    /**
+     * Post constructor initialisation method.
+     */
+    virtual int init() override final;
 
-            /**
-             * A periodic callback invoked by the fiber scheduler idle thread.
-             * We use this for any low priority, background housekeeping.
-             */
-            virtual void idleCallback(){
-                codal_dmesg_flush();
-            }
+    /**
+     * Delay execution for the given amount of time.
+     *
+     * If the scheduler is running, this will deschedule the current fiber and perform
+     * a power efficient, concurrent sleep operation.
+     *
+     * If the scheduler is disabled or we're running in an interrupt context, this
+     * will revert to a busy wait.
+     *
+     * Alternatively: wait, wait_ms, wait_us can be used which will perform a blocking sleep
+     * operation.
+     *
+     * @param milliseconds the amount of time, in ms, to wait for. This number cannot be negative.
+     *
+     */
+    static void sleep(uint32_t milliseconds);
 
-            /**
-             * A periodic callback invoked by the fiber scheduler every SCHEDULER_TICK_PERIOD_MS.
-             */
-            virtual void periodicCallback(){
-            }
+    /**
+     * A periodic callback invoked by the fiber scheduler idle thread.
+     * We use this for any low priority, background housekeeping.
+     */
+    virtual void idleCallback() override final;
 
-            /**
-             * Determine the time since this board was last reset.
-             *
-             * @return The time since the last reset, in milliseconds.
-             *
-             * @note This will value overflow after 1.6 months.
-             */
-            CODAL_TIMESTAMP systemTime(){
-                return system_timer_current_time();
-            }
-    };
+    /**
+     * Determine the time since this board was last reset.
+     *
+     * @return The time since the last reset, in milliseconds.
+     *
+     * @note This will value overflow after 1.6 months.
+     */
+    static CODAL_TIMESTAMP systemTime();
+};
 
-    extern STM32IotNode *default_device_instance;
-}
+extern STM32IotNode* default_device_instance;
+}  // namespace codal
 #endif

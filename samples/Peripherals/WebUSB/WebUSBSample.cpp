2796cb3dd85a6cf9f94ac11c1030e74082a3253a
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-04-15 11:52:37 +0000

    Edit WebUSB sexemple to support CDC

--- samples/Peripherals/WebUSB/WebUSBSample.cpp
+++ samples/Peripherals/WebUSB/WebUSBSample.cpp
@@ -1,4 +1,5 @@
 #include "WebUSBSample.h"
+
 #include "STM32WebUSB.h"
 
 using namespace codal;
@@ -8,20 +9,29 @@ void WebUSBSample_main()
     STM32IotNode iotNode;
     STM32WebUSB webusb;
 
+    webusb.init();
+
+    // We block execution Serial USB is not connected
+    while (!webusb.isConnected()) {
+    }
+
     printf("\n");
     printf("*******************************************\n\r");
     printf("* Demonstration de l'activation du WebUSB *\n\r");
     printf("*******************************************\n\r");
 
-    webusb.init();
-
     bool state = false;
     while (true) {
         iotNode.io.led1.setDigitalValue((int)state);
         iotNode.io.led2.setDigitalValue((int)!state);
-        codal::STM32IotNode::sleep(250);
+
+        webusb.send(".");
+
+        while (webusb.isReadable() > 0) {
+            webusb.send((char)webusb.getChar(ASYNC));
+        }
+
+        codal::STM32IotNode::sleep(1000);
         state = !state;
     }
-
-    webusb.deInit();
 }

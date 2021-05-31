1740016fac0f99a97b5c255f7e1f2a93cac659a2
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-05-31 12:25:29 +0000

    Refactor include guard name

--- samples/Peripherals/BLE/BLE_Peripheral_Sample.h
+++ samples/Peripherals/BLE/BLE_Peripheral_Sample.h
@@ -1,7 +1,7 @@
 #include "STM32IotNode.h"
 
-#ifndef BLE_HCI_SAMPLE_H
-#define BLE_HCI_SAMPLE_H
+#ifndef BLE_PERIPHERAL_SAMPLE_H
+#define BLE_PERIPHERAL_SAMPLE_H
 
 #ifndef SAMPLE_MAIN
 #define SAMPLE_MAIN BLE_HCI_Sample_main

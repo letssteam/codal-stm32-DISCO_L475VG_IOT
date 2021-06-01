3495a2bda781c09fbec44cf78c1a8dc49154c510
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-06-01 14:26:47 +0000

    Create Serial BLE sample

---
+++ samples/Peripherals/BLE Serial/BLE_Serial_Sample.h
@@ -0,0 +1,12 @@
+#include "STM32IotNode.h"
+
+#ifndef BLE_SERIAL_SAMPLE_H
+#define BLE_SERIAL_SAMPLE_H
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN BLE_Serial_Sample_main
+#endif
+
+void BLE_Serial_Sample_main();
+
+#endif

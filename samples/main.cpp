d6bf31a2691d67fe87d90b362f0d8cb9d419a42b
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-06-16 11:21:58 +0000

    Add sample configuration to main

--- samples/main.cpp
+++ samples/main.cpp
@@ -4,14 +4,28 @@
 
 #if defined(GPIO_SAMPLE)
 #include "GPIOSample.h"
-#elif defined(ADC_SAMPLE)
-#include "ADCSample.h"
-#elif defined(HTS221_SAMPLE)
-#include "HTS221_sample.h"
-#elif defined(SERIAL_SAMPLE)
-#include "SerialSample.h"
 #elif defined(PIN_SAMPLE)
 #include "PinSample.h"
+#elif defined(ADC_SAMPLE)
+#include "ADCSample.h"
+#elif defined(SERIAL_SAMPLE)
+#include "SerialSample.h"
+#elif defined(SPI_SAMPLE)
+#include "SpiSample.h"
+#elif defined(HTS221_SAMPLE)
+#include "HTS221_sample.h"
+#elif defined(LPS22HB_SAMPLE)
+#include "LPS22HB_sample.h"
+#elif defined(VL53L0X_SAMPLE)
+#include "VL53L0X_sample.h"
+#elif defined(LIS3MDL_SAMPLE)
+#include "LIS3MDL_sample.h"
+#elif defined(LSM6DSL_SAMPLE)
+#include "LSM6DSL_sample.h"
+#elif defined(LSM6DSL_ACCEL_EVENT_SAMPLE)
+#include "LSM6DSL_accel_sample_event.h"
+#elif defined(VL53L0X_SAMPLE)
+#include "VL53L0X_sample.h"
 #else
 #include "BlinkSample.h"
 #endif

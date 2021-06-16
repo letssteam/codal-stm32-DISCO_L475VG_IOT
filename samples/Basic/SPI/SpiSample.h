7b3c69231a8bfe447f32bda7dad8cd00295f0d51
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-06-16 11:21:34 +0000

    Add SPI sample

---
+++ samples/Basic/SPI/SpiSample.h
@@ -0,0 +1,12 @@
+#include "STM32DISCO_L475VG_IOT.h"
+
+#ifndef SPI_SAMPLE_H
+#define SPI_SAMPLE_H
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN SpiSample_main
+#endif
+
+void SpiSample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot);
+
+#endif

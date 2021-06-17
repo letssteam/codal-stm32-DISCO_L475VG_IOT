67cb9853b3a90b9bcd4d317ce6aab0f89718453b
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-06-17 14:38:02 +0000

    Correct the BLE Serial Sample

--- samples/BLE/BLE_Serial/BLE_Serial_Sample.cpp
+++ samples/BLE/BLE_Serial/BLE_Serial_Sample.cpp
@@ -1,6 +1,7 @@
 #include "BLE_Serial_Sample.h"
 
 #include "STM32SerialBLE.h"
+#include "wiring_time.h"
 
 codal::STM32DISCO_L475VG_IOT_IO io;
 codal::STM32SPI spi3(io.miso3, io.mosi3, io.sclk3);
@@ -24,12 +25,12 @@ void blePeripheralDisconnectHandler(const BLEDevice& central)
 void blePanic()
 {
     printf("Failed to initialize BLE !!\r\n");
-
+    bool state = false;
     while (true) {
         io.led2.setDigitalValue(state ? 1 : 0);
         io.led1.setDigitalValue(state ? 0 : 1);
         state = !state;
-        discoL475VgIot.sleep(50);
+        delay(50);
     }
 }
 
@@ -41,8 +42,6 @@ void BLE_Serial_Sample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
     printf("*******************************************\r\n");
     printf("*       Demonstration du Serial BLE       *\r\n");
     printf("*******************************************\r\n");
-
-    bool state = false;
 
     if (BLE.begin() != 0) {
         printf("BLE Initialized !\r\n");

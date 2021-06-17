e9d3df3b5f7c75bbea7335be568b5dabaad23ce1
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2021-06-17 15:13:46 +0000

    Add WebUSB sample

---
+++ samples/USB/WebUSB/WebUSBSample.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "STM32DISCO_L475VG_IOT.h"
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN WebUSBSample_main
+#endif
+
+void WebUSBSample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot);

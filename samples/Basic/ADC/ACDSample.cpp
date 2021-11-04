48b5113ed0172bb5d262261638102e6e36933cd1
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-11-04 11:21:53 +0000

    Create ADC sample

--- samples/Basic/ADC/ACDSample.cpp
+++ samples/Basic/ADC/ACDSample.cpp
@@ -1,3 +1,20 @@
 #include "ADCSample.h"
+#include "STM32LowLevelTimer.h"
+#include "STM32PWM.h"
 
-void ADCSample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot) {}
+using namespace codal;
+
+void ADCSample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
+{
+    while (true) {
+        for (int i = 0; i < 1024; i++) {
+            discoL475VgIot.io.D2.setAnalogValue(i);
+            discoL475VgIot.sleep(1);
+        }
+
+        for (int i = 0; i < 1024; i++) {
+            discoL475VgIot.io.D2.setAnalogValue(1024 - i);
+            discoL475VgIot.sleep(1);
+        }
+    }
+}

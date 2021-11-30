238cc788a1cfcbdea5dac693a7ddabd71e7eade8
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-11-30 13:44:51 +0000

    Add driver SSD1327

---
+++ samples/Display/OLED_SSD1327/OLED_SSD1327.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "STM32DISCO_L475VG_IOT.h"
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN OledSample1327_main
+#endif
+
+void OledSample1327_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot);

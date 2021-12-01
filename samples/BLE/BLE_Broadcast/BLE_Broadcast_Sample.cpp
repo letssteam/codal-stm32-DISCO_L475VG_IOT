24fcc1d3bd19a24e55ea9a750ef7a29b974e4d7f
Jonathan BAUDIN <jjbaudin@gmail.com>
2021-12-01 18:32:12 +0000

    Remove STM32Duino BLE Usage

--- samples/BLE/BLE_Broadcast/BLE_Broadcast_Sample.cpp
+++ samples/BLE/BLE_Broadcast/BLE_Broadcast_Sample.cpp
@@ -1,53 +1,57 @@
 #include "BLE_Broadcast_Sample.h"
 
-#include "STM32duinoBLE.h"
+#include "STM32BLEAdvertising.h"
 
-codal::STM32DISCO_L475VG_IOT_IO io;
-codal::STM32SPI spi3(io.miso3, io.mosi3, io.sclk3);
-HCISpiTransportClass hci(spi3, SPBTLE_RF, pinNametoDigitalPin(PD_13), pinNametoDigitalPin(PE_6),
-                         pinNametoDigitalPin(PA_8), 8000000, 0);
-BLELocalDevice BLEObj(&hci);
-BLELocalDevice& BLE = BLEObj;
+// #include "STM32duinoBLE.h"
+
+// codal::STM32DISCO_L475VG_IOT_IO io;
+// codal::STM32SPI spi3(io.miso3, io.mosi3, io.sclk3);
+// HCISpiTransportClass hci(spi3, SPBTLE_RF, pinNametoDigitalPin(PD_13), pinNametoDigitalPin(PE_6),
+//                          pinNametoDigitalPin(PA_8), 8000000, 0);
+// BLELocalDevice BLEObj(&hci);
+// BLELocalDevice& BLE = BLEObj;
 
 void BLE_Broadcast_Sample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
 {
-    discoL475VgIot.serial.init(115200);
+    codal::STM32BLEAdvertising advrt();
 
-    printf("\r\n");
-    printf("*******************************************\r\n");
-    printf("*      Demonstration du BLE Broadcast     *\r\n");
-    printf("*******************************************\r\n");
+    // discoL475VgIot.serial.init(115200);
 
-    bool state = false;
+    // printf("\r\n");
+    // printf("*******************************************\r\n");
+    // printf("*      Demonstration du BLE Broadcast     *\r\n");
+    // printf("*******************************************\r\n");
 
-    if (BLE.begin() != 0) {
-        printf("BLE Initialized !\r\n");
-    }
-    else {
-        printf("Failed to initialize BLE !!\r\n");
+    // bool state = false;
 
-        while (true) {
-            io.led2.setDigitalValue(state ? 1 : 0);
-            io.led1.setDigitalValue(state ? 1 : 0);
-            state = !state;
-            discoL475VgIot.sleep(50);
-        }
-    }
+    // if (BLE.begin() != 0) {
+    //     printf("BLE Initialized !\r\n");
+    // }
+    // else {
+    //     printf("Failed to initialize BLE !!\r\n");
 
-    const char* advDataStr = "My broadcast message !";
+    //     while (true) {
+    //         io.led2.setDigitalValue(state ? 1 : 0);
+    //         io.led1.setDigitalValue(state ? 1 : 0);
+    //         state = !state;
+    //         discoL475VgIot.sleep(50);
+    //     }
+    // }
 
-    BLEAdvertisingData advData;
-    advData.setAdvertisedServiceData(0x1800, (uint8_t*)(advDataStr), strlen(advDataStr));
+    // const char* advDataStr = "My broadcast message !";
 
-    BLE.setAdvertisingData(advData);
-    BLE.setLocalName("My Device");
-    BLE.setConnectable(false);
-    BLE.advertise(37);
+    // BLEAdvertisingData advData;
+    // advData.setAdvertisedServiceData(0x1800, (uint8_t*)(advDataStr), strlen(advDataStr));
 
-    while (true) {
-        io.led2.setDigitalValue(state ? 1 : 0);
-        io.led1.setDigitalValue(state ? 0 : 1);
-        state = !state;
-        discoL475VgIot.sleep(150);
-    }
+    // BLE.setAdvertisingData(advData);
+    // BLE.setLocalName("My Device");
+    // BLE.setConnectable(false);
+    // BLE.advertise(37);
+
+    // while (true) {
+    //     io.led2.setDigitalValue(state ? 1 : 0);
+    //     io.led1.setDigitalValue(state ? 0 : 1);
+    //     state = !state;
+    //     discoL475VgIot.sleep(150);
+    // }
 }

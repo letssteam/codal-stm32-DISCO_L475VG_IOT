0203e5702fe4242b1476a0b375968ac55ee2e974
Sébastien NEDJAR <sebastien@nedjar.com>
2022-01-18 14:59:57 +0000

    Add wifi sample

---
+++ samples/WIFI/HttpClient/WIFI_Http_Client_Sample.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "STM32DISCO_L475VG_IOT.h"
+#include "wifi.h"
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN Wifi_Http_Client_Sample_main
+#endif
+
+void Wifi_Http_Client_Sample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot);
--- samples/main.cpp
+++ samples/main.cpp
@@ -42,6 +42,8 @@
 #include "BLE_Serial_Sample.h"
 #elif defined(OLED_SSD1306_SAMPLE)
 #include "OLED_SSD1306.h"
+#elif defined(WIFI_HTTP_CLIENT_SAMPLE)
+#include "WIFI_Http_Client_Sample.h"
 #elif defined(MESSAGEBUS_SAMPLE)
 #include "MessageBus_Sample.h"
 #else

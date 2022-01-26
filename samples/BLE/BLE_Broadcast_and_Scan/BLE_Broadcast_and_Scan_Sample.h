fc6768bf18804059b7544bf410978194e017de15
Jonathan BAUDIN <jjbaudin@gmail.com>
2022-01-26 07:45:16 +0000

    update sample (+ add one)

---
+++ samples/BLE/BLE_Broadcast_and_Scan/BLE_Broadcast_and_Scan_Sample.h
@@ -0,0 +1,12 @@
+#include "STM32DISCO_L475VG_IOT.h"
+
+#ifndef BLE_BROADCAST_AND_SCAN_SAMPLE_H
+#define BLE_BROADCAST_AND_SCAN_SAMPLE_H
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN BLE_Broadcast_and_Scan_sample
+#endif
+
+void BLE_Broadcast_and_Scan_sample(codal::STM32DISCO_L475VG_IOT& discoL475VgIot);
+
+#endif

c7cdce8c5f80033ddc569c32ab6e5adbdc5fb33d
Sébastien NEDJAR <sebastien@nedjar.com>
2022-02-02 15:37:25 +0000

    Update sample

--- samples/WIFI/HttpClient/WIFI_Http_Client_Sample.cpp
+++ samples/WIFI/HttpClient/WIFI_Http_Client_Sample.cpp
@@ -8,12 +8,12 @@ codal::STM32Pin miso3(ID_PIN_WIFI_MISO, PinNumber::PC_11, codal::PIN_CAPABILITY_
 codal::STM32Pin mosi3(ID_PIN_WIFI_MOSI, PinNumber::PC_12, codal::PIN_CAPABILITY_DIGITAL);
 codal::STM32Pin sclk3(ID_PIN_WIFI_SCLK, PinNumber::PC_10, codal::PIN_CAPABILITY_DIGITAL);
 
-codal::STM32SPI spi3(miso3, mosi3, sclk3, 8000000UL, 0, true);
+codal::STM32SPI spi3(miso3, mosi3, sclk3, 10000000UL, 0, true);
 
 IsmDrvClass DrvWiFi(&spi3, PinNumber::PE_0, PinNumber::PE_1, PinNumber::PE_8, PinNumber::PB_13);
 
-const char* ssid       = "Unplugged2";
-const char* passphrase = "erasmusplus";
+const char* ssid       = "SSID";
+const char* passphrase = "PASSWORD";
 
 void Wifi_Http_Client_Sample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
 {
@@ -38,7 +38,13 @@ void Wifi_Http_Client_Sample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
 
     printf("Init Ok\r\n");
 
-    if (DrvWiFi.ES_WIFI_Connect(ssid, passphrase, ES_WIFI_SEC_OPEN) != ES_WIFI_STATUS_OK) {
+    printf("Begin scan network\r\n");
+    DrvWiFi.ES_WIFI_ListAccessPoints();
+    printf("End scan network\r\n");
+
+    printf("Number of network : %d\r\n", DrvWiFi.ES_WIFI_GetApNbr());
+
+    if (DrvWiFi.ES_WIFI_Connect(ssid, passphrase, ES_WIFI_SEC_WPA2) != ES_WIFI_STATUS_OK) {
         printf("Connection to %s not Ok\r\n", ssid);
         DrvWiFi.ES_WIFI_Disconnect();
         return;

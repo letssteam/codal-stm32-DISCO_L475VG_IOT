cf023e46e3f7f84564f35a1106ad4c42ed5c7dec
Sébastien NEDJAR <sebastien@nedjar.com>
2022-02-03 12:16:09 +0000

    Update pin Id

--- samples/WIFI/HttpClient/WIFI_Http_Client_Sample.cpp
+++ samples/WIFI/HttpClient/WIFI_Http_Client_Sample.cpp
@@ -4,18 +4,19 @@
 #include "es_wifi_conf.h"
 #include "ssd1306.h"
 
-codal::STM32Pin miso3(ID_PIN_WIFI_MISO, PinNumber::PC_11, codal::PIN_CAPABILITY_DIGITAL);
-codal::STM32Pin mosi3(ID_PIN_WIFI_MOSI, PinNumber::PC_12, codal::PIN_CAPABILITY_DIGITAL);
-codal::STM32Pin sclk3(ID_PIN_WIFI_SCLK, PinNumber::PC_10, codal::PIN_CAPABILITY_DIGITAL);
+codal::STM32Pin miso3(ID_PIN_WIFI_ISM43362_MISO, PinNumber::PC_11, codal::PIN_CAPABILITY_DIGITAL);
+codal::STM32Pin mosi3(ID_PIN_WIFI_ISM43362_MOSI, PinNumber::PC_12, codal::PIN_CAPABILITY_DIGITAL);
+codal::STM32Pin sclk3(ID_PIN_WIFI_ISM43362_SCK, PinNumber::PC_10, codal::PIN_CAPABILITY_DIGITAL);
 
 codal::STM32SPI spi3(miso3, mosi3, sclk3, 10000000UL, 0, true);
 
-codal::STM32Pin wakeUpPin(ID_PIN_WIFI_WAKE_UP, PinNumber::PB_13, codal::PIN_CAPABILITY_DIGITAL);
-codal::STM32Pin resetPin(ID_PIN_WIFI_RESET, PinNumber::PE_8, codal::PIN_CAPABILITY_DIGITAL);
-codal::STM32Pin nssPin(ID_PIN_WIFI_NSS, PinNumber::PE_0, codal::PIN_CAPABILITY_DIGITAL);
-codal::STM32Pin commandDataReadyPin(ID_PIN_WIFI_COMMAND_DATA_READY, PinNumber::PE_1, codal::PIN_CAPABILITY_DIGITAL);
+codal::STM32Pin wakeUpPin(ID_PIN_WIFI_ISM43362_WAKE_UP, PinNumber::PB_13, codal::PIN_CAPABILITY_DIGITAL);
+codal::STM32Pin resetPin(ID_PIN_WIFI_ISM43362_RESET, PinNumber::PE_8, codal::PIN_CAPABILITY_DIGITAL);
+codal::STM32Pin csPin(ID_PIN_WIFI_ISM43362_CS, PinNumber::PE_0, codal::PIN_CAPABILITY_DIGITAL);
+codal::STM32Pin commandDataReadyPin(ID_PIN_WIFI_ISM43362_COMMAND_DATA_READY, PinNumber::PE_1,
+                                    codal::PIN_CAPABILITY_DIGITAL);
 
-IsmDrvClass DrvWiFi(&spi3, &nssPin, &commandDataReadyPin, &resetPin, &wakeUpPin);
+IsmDrvClass DrvWiFi(&spi3, &csPin, &commandDataReadyPin, &resetPin, &wakeUpPin);
 
 const char* ssid       = "SSID";
 const char* passphrase = "PASSWORD";

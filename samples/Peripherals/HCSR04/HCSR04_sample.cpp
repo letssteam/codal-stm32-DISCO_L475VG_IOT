d4e8ce667bb348b77f51ec87c6c15dfe74a21b1e
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2023-04-20 10:31:30 +0200

    changed distance display and added time display

--- samples/Peripherals/HCSR04/HCSR04_sample.cpp
+++ samples/Peripherals/HCSR04/HCSR04_sample.cpp
@@ -2,39 +2,38 @@
 
 #include <string>
 
-
-void isFar() {
+void isFar()
+{
     printf("tres loin\n\n");
 }
 
-void isNear() {
+void isNear()
+{
     printf("tres proche\n\n");
 }
 
-void basicGetDistance(codal::STM32DISCO_L475VG_IOT& discoL475VgIot) {
+void basicGetDistance(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
+{
     codal::HCSR04 hcsr04(discoL475VgIot.io.D6, discoL475VgIot.io.D7);
 
-    std::string distance1;
-    std::string distance2;
-    std::string distance3;
-    std::string distance4;
-    while(true) {
-        distance1 = "distance = " + std::to_string(hcsr04.getDistance(codal::HCSR04Unit::Mm)) + " mm\n";
-        distance2 = "distance = " + std::to_string(hcsr04.getDistance(codal::HCSR04Unit::Cm)) + " cm\n";
-        distance3 = "distance = " + std::to_string(hcsr04.getDistance(codal::HCSR04Unit::Dm)) + " dm\n";
-        distance4 = "distance = " + std::to_string(hcsr04.getDistance(codal::HCSR04Unit::M)) + " m\n\n";
+    float distance;
+    std::string temps;
+    while (true) {
+        distance = hcsr04.getDistance(codal::HCSR04Unit::Mm);
+        temps    = std::to_string(hcsr04.getTime(codal::HCSR04TimeUnit::Us));
 
-        printf(distance1.c_str());
-        printf(distance2.c_str());
-        printf(distance3.c_str());
-        printf(distance4.c_str());        
-
+        printf(("distance = " + std::to_string(distance) + " mm\n").c_str());
+        printf(("distance = " + std::to_string(distance / 10) + " cm\n").c_str());
+        printf(("distance = " + std::to_string(distance / 100) + " dm\n").c_str());
+        printf(("distance = " + std::to_string(distance / 1000) + " m\n").c_str());
+        printf(("temps = " + temps + "us").c_str());
         discoL475VgIot.sleep(1000);
     }
 }
 
 codal::HCSR04Sensor* hcsr04Sensor;
-void eventIsNearOrFar(codal::STM32DISCO_L475VG_IOT& discoL475VgIot) {
+void eventIsNearOrFar(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
+{
     hcsr04Sensor = new codal::HCSR04Sensor(discoL475VgIot.io.D6, discoL475VgIot.io.D7, 1337);
 
     hcsr04Sensor->registerDistanceEvent(codal::DistanceBehold::Near, 20, codal::HCSR04Unit::Cm, isNear);

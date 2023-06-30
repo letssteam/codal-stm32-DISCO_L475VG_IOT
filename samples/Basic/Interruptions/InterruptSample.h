80754d773fdc6c4d2f6a6e4aee9e918a1680f7f7
Pascal Flores <pascal.flores@etu.univ-amu.fr>
2023-06-30 08:45:47 +0000

    added sample

---
+++ samples/Basic/Interruptions/InterruptSample.h
@@ -0,0 +1,12 @@
+#include "STM32DISCO_L475VG_IOT.h"
+
+#ifndef INTERRUPT_SAMPLE_H
+#define INTERRUPT_SAMPLE_H
+
+#ifndef SAMPLE_MAIN
+#define SAMPLE_MAIN InterruptSample_main
+#endif
+
+void InterruptSample_main(codal::STM32DISCO_L475VG_IOT& discoL475VgIot);
+
+#endif

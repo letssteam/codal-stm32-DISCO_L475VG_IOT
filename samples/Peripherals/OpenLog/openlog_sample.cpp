9f5a204c6ba5f973d6fcd05e74eb50aff7749805
Pascal Flores <pascal.flores@labaixbidouille.com>
2024-04-10 09:08:15 +0000

    refined sample

--- samples/Peripherals/OpenLog/openlog_sample.cpp
+++ samples/Peripherals/OpenLog/openlog_sample.cpp
@@ -2,8 +2,6 @@
 
 #include <cstdio>
 
-#include "Event.h"
-#include "EventModel.h"
 #include "HTS221.h"
 #include "LPS22HB.h"
 #include "openlog.h"
@@ -26,21 +24,18 @@ void openLogSample(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
     printf("*         Demonstration du lecteur OpenLog         *\r\n");
     printf("****************************************************\r\n");
 
-    char* sensorData                = (char*)malloc(100);
-    char* columnsNames              = "temperature (°C);pressure (Pa);humidity (%)\r\n";
-    bool isDataTransmittedCorrectly = openLogWriter->write(columnsNames, strlen(columnsNames));
-    if (!isDataTransmittedCorrectly) {
-        printf("An error occured while sending data to the OpenLog. Exiting :/");
-        return;
-    }
-    float temperature;
-    float pressure;
-    float humidity;
-    int sendResult;
+    char* sensorData   = (char*)malloc(100);
+    char* columnsNames = "temperature (°C);pressure (Pa);humidity (%)\r\n";
+    bool isDataTransferOK;
+
+    isDataTransferOK = openLogWriter->write(columnsNames, strlen(columnsNames));
+    printf("ecriture des en têtes de colonne %s", (isDataTransferOK) ? "reussie" : "echouee :(");
+
+    float temperature, pressure, humidity;
     while (true) {
         temperature = lps22hb.getTemperature();
         pressure    = lps22hb.getPressure();
-        while (!hts221.isTemperatureDataAvailable() || !hts221.isHumidityDataAvailable()) {
+        while (!hts221.isHumidityDataAvailable()) {
             discoL475VgIot.sleep(100);
         }
         humidity = hts221.getHumidity();
@@ -48,8 +43,8 @@ void openLogSample(codal::STM32DISCO_L475VG_IOT& discoL475VgIot)
         unsigned dataSize = snprintf(NULL, 0, "%f;%f;%f\r\n", temperature, pressure, humidity);
         snprintf(sensorData, dataSize, "%f;%f;%f\r\n", temperature, pressure, humidity);
         printf("ecriture des donnees suivantes sur la carte micro-sd :\r\n%s\r\n", sensorData);
-        sendResult = openLogWriter->write(sensorData, dataSize);
-        printf("ecriture %s\r\n\r\n", (sendResult) ? "reussie" : "echouee");
+        isDataTransferOK = openLogWriter->write(sensorData, dataSize);
+        printf("ecriture %s\r\n\r\n", (isDataTransferOK) ? "reussie" : "echouee");
         discoL475VgIot.sleep(1000);
     }
 }

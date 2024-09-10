06ecdcb22f35b1f73889ee1712a425e457962a63
Sébastien NEDJAR <sebastien.nedjar@univ-amu.fr>
2024-09-10 12:08:21 +0000

    Make uniform debug scripts

--- scripts/flash.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-./build.py
-openocd -s "/usr/share/openocd/scripts/" -s"libraries/codal-stm32-DISCO_L475VG_IOT/scripts/" -f "openocd.cfg" -c "program DISCO_L475VG_IOT.hex verify reset exit "
--- scripts/gdb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-arm-none-eabi-gdb --eval "target remote | openocd -s /usr/share/openocd/scripts/ -s libraries/codal-stm32-DISCO_L475VG_IOT/scripts/ -f openocd.cfg -f debug.cfg" build/DISCO_L475VG_IOT
